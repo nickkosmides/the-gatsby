8b57608f70ef57dd993ba3f4cf5697eff694cb80
nick.kosmides <kosmides.nick@gmail.com>
2023-03-09 09:49:17 +0200

    extra

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,6 +26,12 @@ module.exports = {
       resolve: `gatsby-source-wordpress`,
       options: {
         url: process.env.WPGRAPHQL_URL,
+        auth: {
+          htaccess: {
+            username: "flywheel",
+            password: "equal-idea"
+          }
+        }
       },
     },
     {
